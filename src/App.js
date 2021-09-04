944e04e243082a4ea12b07fba51c0d23853cad16
hostem17 <vjjais17@gmail.com>
2021-09-04 20:35:20 +0530

    feed input done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./Header"
 import Sidebar from "./Sidebar"
+import Feed from "./Feed"
 
 
 function App() {
@@ -10,12 +11,15 @@ function App() {
       {/* Header */}
       <Header />
       {/* App Body */}
+
       <div className="app_body">
         <Sidebar />
         {/* sidebar */}
+        <Feed />
         {/* feed */}
         {/* widgets */}
       </div>
+
     </div>
   );
 }
