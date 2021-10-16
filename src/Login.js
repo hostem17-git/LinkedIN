8711dd0f0209bf3304f11245d9032d87087d7eca
hostem17 <vjjais17@gmail.com>
2021-10-17 00:41:54 +0530

    login left done

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,23 @@ import "./Login.css"
 function Login() {
     return (
         <div className="login">
+            <div className="input_buttons">
+                <h1 className="welcome_text">Welcome to My Linked Clone!</h1>
 
+                <div className="input_container">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" ></input>
+                </div>
+
+                <div className="input_container">
+                    <label for="">Description</label>
+                    <input type="text" ></input>
+                </div>
+
+                <button class="submit">Sign in</button>
+            </div>
+            <div className="hero_image">
+            </div>
         </div>
     )
 }
